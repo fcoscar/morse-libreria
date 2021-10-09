60881927be88a4058fb8b4b7ac1500d9a7ed09fe
Oscar <oscarjr01.13@gmail.com>
2021-10-08 20:26:34 -0400

    validation

--- Morse/main.py
+++ Morse/main.py
@@ -3,17 +3,30 @@ morse_code = {'A':'.-', 'B':'-...','C':'-.-.','D':'-..','E':'.',
               'K':'-.-','L':'.-..','M':'--','N':'-.','O':'---','P':'.--.',
               'Q':'--.-','R':'.-.','S':'...','T':'-','U':'..-','V':'...-',
               'W':'.--' ,'X':'-..-','Y':'-.--','Z':'--..', ' ':' '}
-def turnToMorse(string):
-    string_morse = ''
-    for i in range(len(string)):
-        string_morse += morse_code[string[i].upper()] + ';'
-    return string_morse
 
+def isValid(string):
+    if string == "":
+        return False
+    else:
+        return True
+
+
+def turnToMorse(string):
+    if isValid():
+        string_morse = ''
+        for i in range(len(string)):
+            string_morse += morse_code[string[i].upper()] + ';'
+        return string_morse
+    else:
+        return "Invalid Input"
 def turnToString(string):
-    string_normal = ""
-    code = string.split(";")
-    for i in range(len(code)):
-        for key,value in morse_code.items():
-            if code[i] == value:
-                string_normal += key
-    return string_normal
+    if isValid():
+        string_normal = ""
+        code = string.split(";")
+        for i in range(len(code)):
+            for key,value in morse_code.items():
+                if code[i] == value:
+                    string_normal += key
+        return string_normal
+    else:
+        return "Invalid Input"
